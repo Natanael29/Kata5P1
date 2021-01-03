34816b104509f5913538646bae0f30948ccbdf8a
Natanael Martinez Morales <natanael29032000@gmail.com>
2021-01-03 18:27:32 +0000

    Kata5P1 - 2.0 Version

--- src/kata5p1/Kata5P1.java
+++ src/kata5p1/Kata5P1.java
@@ -3,7 +3,11 @@ package kata5p1;
 public class Kata5P1 {
 
     public static void main(String[] args) {
+        //Kata5 1.0 Version
         SelectApp app = new SelectApp();
         app.selectAll();
+        
+        //Kata5 2.0 Version
+        CreateTable.createNewTable();
     }
 }
